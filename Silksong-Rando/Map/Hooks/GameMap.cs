392b92f02c6d548923490c2ff1062318dbbb4733
timothymarriott <timothyrmarriott@gmail.com>
2025-12-21 22:48:08 +0800

    Removed dumb save breaking

--- Silksong-Rando/Map/Hooks/GameMap.cs
+++ Silksong-Rando/Map/Hooks/GameMap.cs
@@ -8,13 +8,6 @@ class GameMap_Start_Patch
     public static void Postfix(GameMap __instance)
     {
         RandoMap.instance.CreateLocationPins();
-        foreach (var scene in GameScenes.Scenes)
-        {
-            GameManager.instance.playerData.scenesMapped.Add(scene);
-        }
-        
-        GameManager.instance.playerData.mapAllRooms = true;
         GameManager.instance.UpdateGameMapWithPopup();
-
     }
 }
